375e62c9eb159d78cc84430e7a4f83f5f75d51fa
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-08-13 14:33:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	this.make="Toyota";
+		this.model = "Corolla";
+	
+}
+getMakeModel(){
+	return `${this.make} + ${this.model}`
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) {
+	super(make,model);
+	this.topSpeed = topSpeed;
+}
+getTopSpeed(){
+	return `1000 km`;
+}
 
 // Do not change the code below
 window.Car = Car;
